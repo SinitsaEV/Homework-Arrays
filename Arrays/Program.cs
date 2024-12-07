44b31f0224d8e0318a745eb74b22c26e8d6caacf
Sinitsa <sinitsammi@gmail.com>
2024-12-07 18:46:49 +0300

    Program

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,17 +7,34 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 
+
             Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
+            Console.Write("Введите число для сдвига: ");
+            int offset = Convert.ToInt32(Console.ReadLine());
 
-            string description = "Unity — это мощная кроссплатформенная игровая движок и среда разработки, " +
-                "созданная компанией Unity Technologies. Он широко используется для создания 2D и 3D игр, " +
-                "а также интерактивных приложений и виртуальной реальности.";
-            string[] descriptionWords = description.Split(' ');
-
-            foreach (string word in descriptionWords)
+            foreach (int i in numbers)
             {
-                Console.WriteLine(word);
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < offset; i++)
+            {
+                int firstNumber = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = firstNumber;
+            }
+
+            foreach(int i in numbers)
+            {
+                Console.Write(i);
             }
         }
     }
