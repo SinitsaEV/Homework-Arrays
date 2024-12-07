6dd9f91ab69819dc346252daaa5017d7013f5d39
Sinitsa <sinitsammi@gmail.com>
2024-12-07 17:34:44 +0300

    Program

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,54 +6,50 @@ namespace Arrays
     internal class Program
     {
         static void Main(string[] args)
-        {
-            const string exitCommand = "exit";
-            const string sumCommand = "sum";
+        {            
+            int[] numbers = new int[30];
+            int maxRandomValue = 5;
+            int minRandomValue = 0;
 
-            int[] numbers = new int[0];
-            int[] tempNumbers; 
-            int sum;
+            int maxRepeatLength = 0;
+            int maxRepeatNumber = 0;
+            int minRepeatLength = 1;
+            int currentRepeatLength = minRepeatLength;
+            int currentRepeatNumber;
 
-            string playerInput;
-            bool exitLoop = true;
+            Random random = new Random();
 
-            Console.OutputEncoding = Encoding.Unicode;
-
-            Console.WriteLine("Команды программы:");
-            Console.WriteLine($"Выйти из программы - {exitCommand}\nВывести сумму всех чисел - {sumCommand}\nВведите любое число для добавления.");
-
-            while (exitLoop)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                playerInput = Console.ReadLine();
-
-                switch (playerInput)
-                {
-                    case exitCommand:
-                        exitLoop = false;
-                        Console.WriteLine("Вы вышли из программы.");
-                        break;
-
-                    case sumCommand:
-                        sum = 0;
-
-                        for (int i = 0; i < numbers.Length; i++)
-                            sum += numbers[i];
-                        
-                        Console.WriteLine("Сумма всех чисел: " + sum);
-                        break;
-
-                    default:
-                        tempNumbers = new int[numbers.Length + 1];
-
-                        for (int i = 0; i < numbers.Length; i++)
-                            tempNumbers[i] = numbers[i];
-
-                        tempNumbers[tempNumbers.Length - 1] = Convert.ToInt32(playerInput);
-                        numbers = tempNumbers;
-                        Console.WriteLine($"Добавлено число: {playerInput}");
-                        break;
-                }
+                numbers[i] = random.Next(minRandomValue, maxRandomValue + 1);
+                Console.Write(numbers[i] + " ");
             }
+
+            currentRepeatNumber = numbers[0];            
+
+            for (int i = 1;i < numbers.Length; i++)
+            {
+                if(currentRepeatNumber == numbers[i])
+                {
+                    currentRepeatLength++;
+                }
+                else
+                {
+                    if(currentRepeatLength > maxRepeatLength)
+                    {
+                        maxRepeatLength = currentRepeatLength;
+                        maxRepeatNumber = currentRepeatNumber;
+                    }
+
+                    currentRepeatNumber = numbers[i];
+                    currentRepeatLength = minRepeatLength;
+                }
+                
+            }
+            
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine();
+            Console.WriteLine($"Число {maxRepeatNumber} встречается {maxRepeatLength} раз подряд.");
         }
     }
 }
